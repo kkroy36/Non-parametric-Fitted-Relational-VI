91866bce00140dc3594dd1d147725a8f6ada24cc
kauroy1994 <kkroy36@gmail.com>
2018-03-08 16:57:49 -0600

    added more results

--- GBFVI/main.py
+++ GBFVI/main.py
@@ -6,9 +6,9 @@ from FVI import FVI
 '''
 #FVI(simulator="logistics",trees=1,loss="Huber",number_of_iterations=20) #logistics default
 #FVI(simulator="blocks",trees=1,loss="Huber",number_of_iterations=20) #blocksworld
-#FVI(simulator="pong") #pong --> uncomment import statements from FVI.py
+FVI(simulator="pong",trees=5,loss="Huber",number_of_iterations=2) #pong --> uncomment import statements from FVI.py
 #FVI(simulator="tetris") #tetris --> uncomment import stmts from FVI.py
 #FVI(simulator="wumpus",trees=1,loss="Huber",number_of_iterations=20) #wumpusworld
 #FVI(simulator="blackjack",trees=1,batch_size=10,loss="Huber",number_of_iterations=20) #no facts in 1 trajectory => (batch_size=10)
-FVI(simulator="50chain",batch_size=3,loss="LAD",number_of_iterations=20) #for simple domains 10 trees not required
+#FVI(simulator="50chain",batch_size=2,trees=1,loss="LS",number_of_iterations=20) #for simple domains 10 trees not required
 #FVI(simulator="net_admin") #network administrator domain
