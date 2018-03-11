67b50dab7c2236f6f069f5b444467c789208046d
kauroy1994 <kkroy36@gmail.com>
2018-03-11 17:00:45 -0500

    added logistics results

--- GBFVI/RelBEs/make_graphs.py
+++ GBFVI/RelBEs/make_graphs.py
@@ -14,7 +14,7 @@ def difference(l1,l2):
 domain = argv[argv.index("-domain")+1]
 
 gbls,gbhuber,gblad,nn,ls,rrt,wtr,wotr = [],[],[],[],[],[],[],[]
-'''
+
 with open(domain+"_GBLSBEs.txt") as f:
     gbls = f.read().splitlines()
     gbls = [item.split(":")[2] for item in gbls]
@@ -24,7 +24,11 @@ with open(domain+"_GBLADBEs.txt") as f:
 with open(domain+"_GBHuberBEs.txt") as f:
     gbhuber = f.read().splitlines()
     gbhuber = [item.split(":")[2] for item in gbhuber]
-'''
+
+with open(domain+"_RRTBEs.txt") as f:
+    rrt = f.read().splitlines()
+    rrt = [item.split(":")[2] for item in rrt]
+
 '''
 with open(domain+"_NNBEs.txt") as f:
     nn = f.read().splitlines()
@@ -33,20 +37,25 @@ with open(domain+"_LSBEs.txt") as f:
     ls = f.read().splitlines()
     ls = [item.split("[")[1][:-1] for item in ls]
 '''
-
 with open(domain+"_transferBEs.txt") as f:
     file = f.read().splitlines()
     file = [float(item.split(":")[2]) for item in file]
-    wotr = file[:20]
-    wtr = file[20:]
+    wotr = file[:100]
+    wtr = file[100:]
     
-
-plt.ylim(ymin=-0.02,ymax=0.2)
-plt.plot(range(20),wotr,label="without transfer",color='red',linewidth=3)
-plt.plot(range(20),wtr,label="with transfer",color='green',linewidth=3)
-#plt.plot(range(20),difference(gbhuber,gbls),label="LAD",color='red',linewidth=3)
-#plt.plot(range(20),difference(gbhuber,ls),label="LS",color='green',linewidth=3)
-
+'''
+print (sum([int(float(x)>3) for x in gbhuber]))
+print (sum([int(float(x)>3) for x in gbls]))
+print (sum([int(float(x)>3) for x in gblad]))
+'''
+#print (sum([int(float(x)>3) for x in wotr]))
+#print (sum([int(float(x)>3) for x in wtr]))
+plt.ylim(ymin=-10,ymax=10)
+#plt.plot(range(100),difference(rrt,gbhuber),label="LS",color='blue',linewidth=3)
+#plt.plot(range(100),difference(gbls,gbhuber),label="LS",color='green',linewidth=3)
+#plt.plot(range(100),difference(gblad,gbhuber),label="LAD",color='red',linewidth=3)
+plt.plot(range(100),wtr,label="with transfer",color='green',linewidth=3)
+plt.plot(range(100),wotr,label="without transfer",color='red',linewidth=3)
 plt.xlabel("iterations")
 plt.ylabel("average of the bellman error")
 plt.title(domain)
