300038c48e3a550d78cd2b86dbd8fe3cf7af47d5
kauroy1994 <kkroy36@gmail.com>
2018-03-11 19:54:09 -0500

    added blackjack results

--- GBFVI/main.py
+++ GBFVI/main.py
@@ -8,7 +8,7 @@ from FVI import FVI
 #FVI(simulator="blocks",transfer=1,trees=10,number_of_iterations=100) #blocksworld
 #FVI(simulator="pong",batch_size=2,trees=1,loss="LS",number_of_iterations=20) #pong --> uncomment import statements from FVI.py
 #FVI(simulator="tetris",trees=1,loss="LS",number_of_iterations=20) #tetris --> uncomment import stmts from FVI.py
-FVI(simulator="wumpus",transfer=1,trees=10,number_of_iterations=100) #wumpusworld
-#FVI(simulator="blackjack",trees=1,batch_size=10,loss="Huber",number_of_iterations=20) #no facts in 1 trajectory => (batch_size=10)
+#FVI(simulator="wumpus",transfer=1,trees=10,number_of_iterations=100) #wumpusworld
+FVI(simulator="blackjack",transfer=1,number_of_iterations=100) #no facts in 1 trajectory => (batch_size=10)
 #FVI(simulator="50chain",batch_size=2,trees=1,loss="LS",number_of_iterations=20) #for simple domains 10 trees not required
 #FVI(simulator="net_admin") #network administrator domain
