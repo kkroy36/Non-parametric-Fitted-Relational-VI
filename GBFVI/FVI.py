af6b7a878ec5bcf08ca290480f92b629761d9d82
Das <sxd170431@utdallas.edu>
2019-02-05 19:55:58 -0600

    Recent changes and average accross runs

--- GBFVI/FVI.py
+++ GBFVI/FVI.py
@@ -1,4 +1,5 @@
 from box_world import Logistics
+import os
 #from wumpus import Wumpus
 #from blocks import Blocks_world
 #from blackjack import Game
@@ -9,11 +10,11 @@ from box_world import Logistics
 from time import clock
 from copy import deepcopy
 from GradientBoosting import GradientBoosting
-
+path="C://Users//sxd170431//Desktop//Work//Projects//Relational_RL//Results//"
 
 class FVI(object):
     """Settings for experiments batch_size=10, no_of_iterations=50, trees=1 (baseline), 3, 5, no_of_runs=5 & 10"""
-    def __init__(self, transfer=0, simulator="logistics", batch_size=10, number_of_iterations=50, loss="LS", trees=10):
+    def __init__(self, transfer=0, simulator="logistics", batch_size=10, number_of_iterations=50, loss="LS", trees=10,path=path,runs=5, policy=0.9,run_latest=0):
         '''transfer = 1, means a prespecified number of iterations are run and learning
            the regression model using RFGB, (relational model) before starting fitted
            value iteration with the learned values
@@ -26,9 +27,28 @@ class FVI(object):
         self.number_of_iterations = number_of_iterations
         self.model = None
         self.state_number = 1
+        self.current_run=run_latest
+        
+        """These are the statistics that needs to be averaged accross runs"""
+        self.bellman_error=[]
+        
+        """These contain true and infered Q(s,a) values for every state action pair in the test trajectory"""
+        self.true_state_action_val=[]
+        self.inf_state_action_val=[]
+        self.test_error_state_action=[]
+        
+        """These contain true and infered Q(s,a) values for just the first state action pair in the test trajectory"""
+        self.true_start_state_action_val=[]
+        self.inf_start_state_action_val=[]
+        self.test_start_error_state_action=[]
+        
+        """This is the path where results for a particular run would be stored"""
+        self.resultpath=path+self.simulator+"//Runs_"+str(runs)+"//Policy_"+str(policy)+"//trees_"+str(self.trees)+"//"+self.loss+"//"
+        
         self.compute_transfer_model()
+                
 
-    def compute_value_of_trajectory(self, values, trajectory, discount_factor=0.99, goal_value=1.0, AVI=False, testCompute=False):
+    def compute_value_of_trajectory(self, values, trajectory, discount_factor=0.99, goal_value=1.0, AVI=False):
         '''computes the value of a trajectory
            by value iteration until convergence
         '''
@@ -61,33 +81,13 @@ class FVI(object):
             
         elif AVI:  # perform value iteration by infering value of next state for value iteration from fiitted model
             for i in range(number_of_transitions-1):
-                test_value_of_state = 0
-                if testCompute:
-                    if i == 0:
-                        next_state_value = goal_value
-                        current_state_number = reversed_trajectory[i][0]
-                        current_state = reversed_trajectory[i][1][:-1]
-                        current_action = reversed_trajectory[i][1][-1]
-                        test_value_of_state = immediate_reward + discount_factor * next_state_value  # V(S) = R(S) + gamma*goal_value
-                        key = (current_state_number, tuple(current_state[:-1]))
-                        values[current_action][key] = test_value_of_state
-                    else:
-                        next_state_number = reversed_trajectory[i-1][0]
-                        next_state = reversed_trajectory[i-1][1][:-1]
-                        next_state_action = reversed_trajectory[i-1][1][-1]
-                        next_state_value = values[next_state_action][(next_state_number, tuple(next_state[:-1]))]
-                        current_state_number = reversed_trajectory[i][0]
-                        current_state = reversed_trajectory[i][1][:-1]
-                        current_action = reversed_trajectory[i][1][-1]
-                        test_value_of_state = immediate_reward + discount_factor*next_state_value #V(S) = R(S) + gamma*V(S')
-                        key = (current_state_number, tuple(current_state[:-1]))
-                        values[current_action][key] = test_value_of_state
                 state_number = trajectory[i][0]
                 state = trajectory[i][1][:-1]
                 state_action = trajectory[i][1][-1]
                 next_state_number = trajectory[i+1][0]
                 next_state = trajectory[i+1][1][:-1]
                 next_state_action = trajectory[i+1][1][-1]
+                #print 'current_state,current_action.next_state,next_state_action',state,state_action,next_state,next_state_action
                 facts = list(next_state)
                 examples = [next_state_action+" "+str(0.0)]
                 value_of_next_state = 0.0
@@ -97,17 +97,92 @@ class FVI(object):
                 except:
                     value_of_next_state = 0.0
                 value_of_state = immediate_reward + discount_factor * value_of_next_state  # V(S) = R(S) + gamma*V_hat(S')
-                if testCompute:
-                    with open("testing_error.txt","a") as fp:
-                        fp.write(str(abs(value_of_state - test_value_of_state))+"\n")
-                    with open("true_values.txt","a") as fp:
-                        fp.write(str(test_value_of_state)+"\n")
-                    with open("inferred_values.txt","a") as fp:
-                        fp.write(str(value_of_state)+"\n")
                 key = (state_number, tuple(state[:-1]))
                 values[state_action][key] = value_of_state
                 
-
+    def compute_value_of_test_trajectory(self, values, trajectory, discount_factor=0.99, goal_value=1.0, AVI=False):
+        '''computes the value of a trajectory
+           by value iteration until convergence
+        '''
+        reversed_trajectory = trajectory[::-1]
+        number_of_transitions = len(reversed_trajectory)
+        immediate_reward = -1
+        
+        """Calculate the original Q-values from Bellman backup equation"""
+        for i in range(number_of_transitions):
+            if i == 0:
+                next_state_value = goal_value
+                current_state_number = reversed_trajectory[i][0]
+                current_state = reversed_trajectory[i][1][:-1]
+                current_action = reversed_trajectory[i][1][-1]
+                value_of_state = immediate_reward + discount_factor * next_state_value  # V(S) = R(S) + gamma*goal_value
+                key = (current_state_number, tuple(current_state[:-1]))
+                #print 'current_action,state',current_action,key
+                values[current_action][key] = value_of_state
+            else:
+                next_state_number = reversed_trajectory[i-1][0]
+                next_state = reversed_trajectory[i-1][1][:-1]
+                next_state_action = reversed_trajectory[i-1][1][-1]
+                next_state_value = values[next_state_action][(next_state_number, tuple(next_state[:-1]))]
+                current_state_number = reversed_trajectory[i][0]
+                current_state = reversed_trajectory[i][1][:-1]
+                current_action = reversed_trajectory[i][1][-1]
+                value_of_state = immediate_reward + discount_factor*next_state_value #V(S) = R(S) + gamma*V(S')
+                key = (current_state_number, tuple(current_state[:-1]))
+                #print 'current_action,state',current_action,key
+                values[current_action][key] = value_of_state
+        #print "Length of trajectory", len(trajectory)
+        #raw_input()
+        """Calculate the infered Q-values from the Fitted Value Iteration model, also calculate the error"""                                 
+     
+        for i in range(number_of_transitions-1):
+            state_number = trajectory[i][0]
+            state = trajectory[i][1][:-1]
+            state_action = trajectory[i][1][-1]
+            next_state_number = trajectory[i+1][0]
+            next_state = trajectory[i+1][1][:-1]
+            next_state_action = trajectory[i+1][1][-1]
+            #print 'current_state,current_action.next_state,next_state_action',state,state_action,next_state,next_state_action
+            facts = list(next_state)
+            examples = [next_state_action+" "+str(0.0)]
+            value_of_next_state = 0.0
+            try:
+                self.model.infer(facts, examples)
+                value_of_next_state = self.model.testExamples[next_state_action.split('(')[0]][next_state_action]
+            except:
+                value_of_next_state = 0.0
+            infered_state_value = immediate_reward + discount_factor * value_of_next_state  # V(S) = R(S) + gamma*V_hat(S')
+            key = (state_number, tuple(state[:-1]))
+            #print 'current_action,state,next_state_action,next_state',state,state_action, next_state,next_state_action
+            #raw_input()
+            true_state_value=values[state_action][key]
+            
+            """Q(s,a) for all state action pairs in the test trajectory"""
+            self.true_state_action_val.append(true_state_value)
+            self.inf_state_action_val.append(infered_state_value)
+            self.test_error_state_action.append(abs(infered_state_value - true_state_value))
+            
+            """Q(s,a) for the first state action pair in the test trajectory"""
+            if (i==0):
+               
+               print "The value of i is",i 
+               self.true_start_state_action_val.append(true_state_value)
+               self.inf_start_state_action_val.append(infered_state_value)
+               self.test_start_error_state_action.append(abs(infered_state_value - true_state_value))
+               with open(self.resultpath+"testing_error_start.txt","a") as fp:
+                    fp.write(str(abs(infered_state_value - true_state_value))+"\n")
+               with open(self.resultpath+"true_values_start.txt","a") as fp:
+                    fp.write(str(true_state_value)+"\n")
+               with open(self.resultpath+"inferred_values_start.txt","a") as fp:
+                    fp.write(str(infered_state_value)+"\n") 
+            
+            with open(self.resultpath+"testing_error.txt","a") as fp:
+                fp.write(str(abs(infered_state_value - true_state_value))+"\n")
+            with open(self.resultpath+"true_values.txt","a") as fp:
+                fp.write(str(true_state_value)+"\n")
+            with open(self.resultpath+"inferred_values.txt","a") as fp:
+                fp.write(str(infered_state_value)+"\n")
+        
     def get_targets(self, examples):
         '''returns the targets'''
         return list(set([item.split('(')[0] for item in examples]))
@@ -131,6 +206,17 @@ class FVI(object):
            Note that in the transfer start state, parameters to allow different grid sizes,
            lets say for wumpus world can be set during object call if allowable by the constructor.
         '''
+        
+        """Creates separate run directories in the destination folder to store results from each run"""
+        dirName=self.resultpath+"//Run"+str(self.current_run)
+        if not os.path.exists(self.resultpath+"//Run"+str(self.current_run)):
+           os.makedirs(dirName)
+           print("Directory " , dirName ,  " Created ")
+        else:    
+           print("Directory " , dirName ,  " already exists")   
+        
+        self.resultpath=dirName+"//"
+        
         facts, examples, bk, reward_function = [], [], [], []
         i = 0
         values = {}
@@ -213,7 +299,7 @@ class FVI(object):
                         within_time = False
                         break
                 if within_time:
-                    print "The trajectory is",trajectory
+                    #print "The trajectory is",trajectory
                     self.init_values(values, trajectory)
                     self.compute_value_of_trajectory(values, trajectory)
                     self.state_number += len(trajectory)+1
@@ -288,7 +374,7 @@ class FVI(object):
                     state = Admin(number=self.state_number, start=True)
                     if not bk:
                         bk = Admin.bk
-                with open(self.simulator+"_FVI_out.txt", "a") as fp:
+                with open(self.resultpath+self.simulator+"_FVI_out.txt", "a") as fp:
                     fp.write("*"*80+"\nstart state: " +
                              str(state.get_state_facts())+"\n")
                     time_elapsed = 0
@@ -330,17 +416,18 @@ class FVI(object):
                             break
                         elif self.simulator == "net_id" and time_elapsed > 1:
                             within_time = False
-                        #print "The value of a trajectory is", trajectory    
+                    #print "The  trajectory is", trajectory    
                     if within_time:
                         self.init_values(values, trajectory)
                         if i == 0:
                             self.compute_value_of_trajectory(
-                                values, trajectory, AVI=True, testCompute=False)
+                                values, trajectory, AVI=True)
                         else:
                             # perform computation using fitted value iteration
                             self.compute_value_of_trajectory(
-                                values, trajectory, AVI=True, testCompute=False)
+                                values, trajectory, AVI=True)
                         self.state_number += 1
+                        print "The value function is", values
                         for key in values:
                             if values[key]:
                                 examples_string = key
@@ -353,7 +440,8 @@ class FVI(object):
             # self.model.infer(facts,examples)
             fitted_values = self.model.infer(facts, examples)
             bellman_error = self.compute_bellman_error(values)
-            with open(self.simulator+"_BEs.txt", "a") as f:
+            self.bellman_error.append(bellman_error)
+            with open(self.resultpath+self.simulator+"_BEs.txt", "a") as f:
                 f.write("iteration: "+str(i) +
                         " average bellman error: "+str(bellman_error)+"\n")
             examples = []
@@ -366,8 +454,9 @@ class FVI(object):
             targets = self.get_targets(examples)
             self.model.setTargets(targets)
             self.model.learn(facts, examples, bk)
-        for i in range(20): #test trajectories
-            if self.simulator == "logistics":
+        i = 0 
+        while i < 20: #test trajectories for logistics
+            if self.simulator == "logistics": # Add other domains specific to testing
                 state = Logistics(number=self.state_number, start=True)
                 time_elapsed = 0
                 within_time = True
@@ -382,10 +471,17 @@ class FVI(object):
                     trajectory.append((s_number, s_facts+[action]))
                     end = clock()
                     time_elapsed = abs(end-start)
-                    if self.simulator == "logistics" and time_elapsed > 0.5:
+                    if self.simulator == "logistics" and time_elapsed > 1:
                         within_time = False
+                        i=i-1
                         break
                 if within_time:
+                    #print "************* i is", i
+                    #print "The  test trajectory is", trajectory 
+                    #raw_input()
                     self.init_values(values, trajectory)
-                    self.compute_value_of_trajectory(values, trajectory, AVI=True, testCompute=True)
+                    self.compute_value_of_test_trajectory(values, trajectory, AVI=True)
                     self.state_number += 1
+            i += 1
+        
+                    
--- GBFVI/main.py
+++ GBFVI/main.py
@@ -1,18 +1,78 @@
 from FVI import FVI
 import sys
+import numpy as np
 '''
    loss can be LS,LAD or Huber
    transfer can be 0 or 1
    number_of_iterations can be set (default=10)
 '''
 #log_file = open("log.txt","w")
+
 #sys.stdout = log_file
-FVI(simulator="logistics",trees=1,batch_size=1,number_of_iterations=1) #logistics default
-#FVI(simulator="blocks",trees=1,batch_size=3,number_of_iterations=2) #blocksworld
-#FVI(simulator="pong",batch_size=2,trees=1,loss="LS",number_of_iterations=20) #pong --> uncomment import statements from FVI.py
-#FVI(simulator="tetris",trees=1,loss="LS",number_of_iterations=20) #tetris --> uncomment import stmts from FVI.py
-#FVI(simulator="wumpus",trees=3,batch_size=3,number_of_iterations=5) #wumpusworld
-#FVI(simulator="blackjack",transfer=1,number_of_iterations=100) #no facts in 1 trajectory => (batch_size=10)
-#FVI(simulator="50chain",batch_size=2,trees=1,loss="LS",number_of_iterations=20) #for simple domains 10 trees not required
-#FVI(simulator="net_admin",trees=1,batch_size=6,number_of_iterations=1) #network administrator domain
-#log_file.close()
+"""These parameters have to be mentioned by the user"""
+
+"""Settings for experiments batch_size=10, no_of_iterations=50, trees=1 (baseline), 3, 5, no_of_runs=5 & 10"""
+"""Sample path: C:\Users\sxd170431\Desktop\Work\Projects\Relational_RL\Results\logistics\Runs_1\Policy_0.9\trees_3\LS"""
+
+path="C://Users//sxd170431//Desktop//Work//Projects//Relational_RL//Results//"
+no_of_runs=2
+policy=0.9
+no_of_state_actions_average=30
+
+"""Data structures for capturing values accross the runs"""
+all_run_bellman_error=[]
+all_run_test_error=[]
+all_run_true_values=[]
+all_run_infered_values=[]
+all_run_test_error_start=[]
+all_run_true_values_start=[]
+all_run_infered_values_start=[]
+np.set_printoptions(threshold=np.inf)
+
+for run in range(0,no_of_runs):
+  print "Beginning run no", run  
+  model=FVI(simulator="logistics",trees=3,batch_size=5,number_of_iterations=10, path=path,runs=no_of_runs, policy=policy,run_latest=run,loss="LS") #logistics default
+  """Statistics for a single run"""
+  all_run_bellman_error.append(model.bellman_error)
+  
+  """Averages over first 50 (s,a) pairs. This variable can be changes"""
+  all_run_test_error.append(model.test_error_state_action[0:no_of_state_actions_average])
+  all_run_true_values.append(model.true_state_action_val[0:no_of_state_actions_average])
+  all_run_infered_values.append(model.inf_state_action_val[0:no_of_state_actions_average])
+  """Statistics for keeping the Q(s,a) of the start state of every trajectory"""
+  all_run_test_error_start.append(model.test_start_error_state_action)
+  all_run_true_values_start.append(model.true_start_state_action_val)
+  all_run_infered_values_start.append(model.inf_start_state_action_val)
+  #FVI(simulator="blocks",trees=1,batch_size=3,number_of_iterations=2) #blocksworld
+  #FVI(simulator="pong",batch_size=2,trees=1,loss="LS",number_of_iterations=20) #pong --> uncomment import statements from FVI.py
+  #FVI(simulator="tetris",trees=1,loss="LS",number_of_iterations=20) #tetris --> uncomment import stmts from FVI.py
+  #FVI(simulator="wumpus",trees=3,batch_size=3,number_of_iterations=5) #wumpusworld
+  #FVI(simulator="blackjack",transfer=1,number_of_iterations=100) #no facts in 1 trajectory => (batch_size=10)
+  #FVI(simulator="50chain",batch_size=2,trees=1,loss="LS",number_of_iterations=20) #for simple domains 10 trees not required
+  #FVI(simulator="net_admin",trees=1,batch_size=6,number_of_iterations=1) #network administrator domain
+  
+resultpath=path+model.simulator+"//Runs_"+str(no_of_runs)+"//Policy_"+str(policy)+"//trees_"+str(model.trees)+"//"+model.loss+"//"
+print all_run_test_error
+"""Average the results across each run"""
+all_run_bellman_error_avg=np.mean(all_run_bellman_error,axis=0)
+all_run_test_error_avg=np.mean(all_run_test_error,axis=0)
+all_run_true_values_avg=np.mean(all_run_true_values,axis=0)
+all_run_infered_values_avg=np.mean(all_run_infered_values,axis=0)
+all_run_test_error_start_avg=np.mean(all_run_test_error_start,axis=0)
+all_run_true_values_start_avg=np.mean(all_run_true_values_start,axis=0)
+all_run_infered_values_start_avg=np.mean(all_run_infered_values_start,axis=0)
+
+"""Write all the statistics to a text file"""
+np.savetxt(resultpath+'avg_bellman_error.txt',all_run_bellman_error_avg)
+np.savetxt(resultpath+'avg_test_error.txt',all_run_test_error_avg)
+np.savetxt(resultpath+'avg_true_val.txt',all_run_true_values_avg)
+np.savetxt(resultpath+'avg_inf_val.txt',all_run_infered_values_avg)
+np.savetxt(resultpath+'avg_test_error_start.txt',all_run_test_error_start_avg)
+np.savetxt(resultpath+'avg_true_val_start.txt',all_run_true_values_start_avg)
+np.savetxt(resultpath+'avg_inf_val_start.txt',all_run_infered_values_start_avg)
+
+
+
+
+
+
